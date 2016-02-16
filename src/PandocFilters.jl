fd6d852d6fee3b1d16a8b540ffc83abc19d9b1cb
Martin Vuk <martin.vuk@fri.uni-lj.si>
2016-02-16 02:36:54 +0100

    Basic walk function and tests.

--- src/PandocFilters.jl
+++ src/PandocFilters.jl
@@ -1,5 +1,44 @@
+
 module PandocFilters
 
-# package code goes here
+export walk
+
+using JSON
+
+"""
+Type representing Pandoc elements.
+"""
+type PandocElement
+end
+
+"""
+Function walk will walk `Pandoc` document abstract source tree (AST) and apply filter function on each elemnet of the document AST. 
+"""
+function walk(x :: Any, action :: Function)
+    return x
+end
+
+function walk(array :: Array, action :: Function)
+    [walk(elt,action) for elt in array] 
+end
+
+function walk(dict :: Dict, action :: Function)
+    if haskey(dict,"t") & haskey(dict, "c")
+        result = action(dict["t"],dict["c"])
+        if result == nothing
+            Dict("t"=>dict["t"], "c" => walk(dict["c"],action))
+        elseif (typeof(result) <: Dict) && haskey(result,"t") && haskey(result,"c")
+            Dict("t"=>result["t"], "c"=>walk(result["c"],action))
+        else
+            walk(result,action)
+        end
+    else
+        [key=>walk(value,action) for (key,value) in dict]
+    end
+end
+
+function filter(action :: Function)
+    return () -> JSON.print(STDOUT,walk(JSON.parse(STDIN),action))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,23 @@
 using PandocFilters
-using Base.Test
+using FactCheck
 
 # write your own tests here
-@test 1 == 1
+facts("Testing walk with (t,c)->1") do
+    action(t,c) = 1
+    @fact walk(1,action) --> 1
+    @fact walk([1,2,3],action) --> [1,2,3]
+    @fact walk(Dict("x"=>2,"y"=>3),action) --> Dict("x"=>2,"y"=>3)
+    @fact walk(Dict("t"=>2,"c"=>3),action) --> 1
+end
+
+facts("Testing walk with t=>x -> t=>y") do
+    action(t,c) = (t=="x")?Dict("t"=>"y","c"=>c):nothing
+    dict_x = Dict("t"=>"x","c"=>"z")
+    dict_y = Dict("t"=>"y","c"=>"z")
+    dict_z = Dict("t"=>"z","c"=>"w")
+    @fact walk(dict_x,action) --> dict_y
+    @fact walk([dict_x,dict_y,dict_z],action) --> [dict_y,dict_y,dict_z]
+    @fact walk(Dict("t"=>"w","c"=>dict_x),action) --> Dict("t"=>"w","c"=>dict_y)
+    @fact walk([dict_z,Dict("t"=>"w","c"=>dict_x)],action) --> [dict_z,Dict("t"=>"w","c"=>dict_y)]
+    
+end
